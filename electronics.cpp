003daf10a1388ec35237d99aed4b8758ca407591
ans29 <anshul.sharma1995@gmail.com>
2019-05-23 17:50:01 +0530

    add func to electronics.cpp

--- electronics.cpp
+++ electronics.cpp
@@ -7,24 +7,64 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 class Eelectronics
 {
 	int screen_size;
 	int price;
 	int battery;
+
+	string basicToString()
+	{
+		string s = to_string (screen_size);
+		s = s.append (":");
+		s = s.append (to_string (price));
+		s = s.append (":");
+		s = s.append (to_string (battery));
+		return s;
+	}
+
+   	public:
+		string toString();
 };
 
 class MobilePhone : public Eelectronics
 {
-
+	int camera_resolution;
+	string toString()
+	{
+		string s = basicToString();
+		s = s.append (":")
+		s = s.append (to_string (camera_resolution));
+		return s;
+	}
 };
 
 class Tablet : public MobilePhone
 {
-
+	int processor; 
+	string toString()
+	{
+		string s = basicToString();
+		s = s.append (":")
+		s = s.append (to_string (camera_resolution));
+		s = s.append (":")
+		s = s.append (to_string (processor));
+		return s;
+	}
 };
 
 class Laptop : public Eelectronics
 {
-
+	string processor, graphic_card;
+	string toString()
+	{
+		string s = basicToString();
+		s = s.append (":")
+		s = s.append (processor);
+		s = s.append (":")
+		s = s.append (graphic_card);	
+		return s;
+	}
 };
